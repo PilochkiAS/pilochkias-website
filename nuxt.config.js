0256c08e9f0219158653f330416548b0b61b9f2a
OleMan <aom-95@live.com>
2018-11-07 23:24:54 +0200

    theme-color meta tag added

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' },
+      { hid: 'theme-color', name: 'theme-color', content: '#C2185B' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
