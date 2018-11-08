b9142880c81f4a7633d297e50be26478a45be00f
OleMan <aom-95@live.com>
2018-11-09 01:21:22 +0200

    products toolbar

--- pages/products.vue
+++ pages/products.vue
@@ -1,9 +1,29 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      </div>
+      <v-toolbar class="mb-4">
+        <v-breadcrumbs icons class="px-0">
+          <v-breadcrumbs-item
+                  v-for="item in items"
+                  :key="item.text"
+                  :disabled="item.disabled"
+          >
+            <v-icon v-if="item.isHome">home</v-icon>
+            {{ item.text }}
+          </v-breadcrumbs-item>
+          <v-icon slot="divider" class="px-0">chevron_right</v-icon>
+        </v-breadcrumbs>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn icon class="mx-sm-0"><v-icon>filter_list</v-icon></v-btn>
+          <v-btn icon class="mx-sm-0"><v-icon>swap_vert</v-icon></v-btn>
+          <v-btn icon class="mx-sm-0">
+            <v-icon v-if="isModuleList" @click="isModuleList = !isModuleList">view_module</v-icon>
+            <v-icon v-else @click="isModuleList = !isModuleList">view_list</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
         <v-card-text>
@@ -28,3 +48,78 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            text: '',
+            isHome: true,
+            disabled: false,
+            href: '/'
+          },
+          {
+            text: 'Товары',
+            disabled: true,
+            href: '/products'
+          }
+        ],
+        isModuleList: true
+      }
+    },
+    created () {
+      this.handleHash(this.$route.hash)
+    },
+    methods: {
+      handleHash (hash) {
+        switch (hash) {
+          case '#all':
+            this.items[1].text = 'Вся продукция'
+            this.items[1].link = '/products#all'
+            this.items[1].disabled = false
+            break
+          case '#saw-files':
+            this.items[1].text = 'Пилки и файлы'
+            this.items[1].link = '/products#saw-files'
+            this.items[1].disabled = false
+            break
+          case '#smart-disks':
+            this.items[1].text = 'Диски SMart'
+            this.items[1].link = '/products#smart-disks'
+            this.items[1].disabled = false
+            break
+          case '#equipment':
+            this.items[1].text = 'Оборудование'
+            this.items[1].link = '/products#equipment'
+            this.items[1].disabled = false
+            break
+        }
+      }
+    },
+    watch: {
+      $route (to, from) {
+        this.handleHash(to.hash)
+      }
+    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .v-breadcrumbs li:nth-child(even) {
+    padding: 0 6px;
+  }
+
+  @media screen and (max-width: 960px) {
+    .v-breadcrumbs li:nth-child(even) {
+      padding: 0 6px;
+    }
+    .mx-sm-0 {
+      margin-left: 0;
+      margin-right: 0;
+    }
+  }
+</style>
