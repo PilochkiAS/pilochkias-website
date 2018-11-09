cd6ffd99fa9d9bf27642598430dbe4e199f3df7d
OleMan <aom-95@live.com>
2018-11-09 12:03:34 +0200

    add products page content

---
+++ components/ProductsContent.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-container grid-list-lg fluid class="px-0">
+        <v-layout :column="!isModuleList" wrap>
+          <v-flex xs4 v-for="n in 18" :key="n">
+            <v-card flat tile hover>
+              <!--<v-card-title>-->
+                <!--<div>-->
+                  <!--<div class="headline">Top western road trips</div>-->
+                <!--</div>-->
+              <!--</v-card-title>-->
+
+              <v-layout :column="isModuleList">
+                <v-flex xs5 class="py-0">
+                  <!--<v-img-->
+                          <!--:src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"-->
+                          <!--height="150px"-->
+                  <!--&gt;</v-img>-->
+                  <v-img
+                          src="https://smart-pilka.ru/image/cache/catalog/img_6425-1000x1000.jpg"
+                          height="150px"
+                  ></v-img>
+                </v-flex>
+
+                <v-flex xs7>
+                  <v-card-title primary-title class="py-0">
+                    <div>
+                      <div class="title">SMart лампа 56 Ватт светодиодная</div>
+                      <span class="grey--text">1,000 miles of wonder</span>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+
+              <v-divider light inner></v-divider>
+
+              <v-card-actions>
+                <div class="accent--text font-weight-bold">8 800.00 грн</div>
+                <v-spacer></v-spacer>
+                <v-btn icon class="action-btn">
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+                <v-btn icon class="action-btn">
+                  <v-icon>share</v-icon>
+                </v-btn>
+                <v-btn icon class="action-btn">
+                  <v-icon>add_shopping_cart</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        products: [
+          { title: 'Вся продукция', to: '/products#all' }
+        ]
+      }
+    },
+    props: ['isModuleList']
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .v-btn.action-btn:hover .v-btn__content .v-icon {
+    color: #536DFE;
+  }
+</style>
--- pages/products.vue
+++ pages/products.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-layout row justify-center>
+    <v-flex xs12 lg12 md12>
       <v-toolbar class="mb-4">
         <v-breadcrumbs icons class="px-0">
           <v-breadcrumbs-item
-                  v-for="item in items"
+                  v-for="item in breadcrumbs"
                   :key="item.text"
                   :disabled="item.disabled"
           >
@@ -18,42 +18,24 @@
           <v-btn icon class="mx-sm-0"><v-icon>filter_list</v-icon></v-btn>
           <v-btn icon class="mx-sm-0"><v-icon>swap_vert</v-icon></v-btn>
           <v-btn icon class="mx-sm-0">
-            <v-icon v-if="isModuleList" @click="isModuleList = !isModuleList">view_module</v-icon>
+            <v-icon v-if="!isModuleList" @click="isModuleList = !isModuleList">view_module</v-icon>
             <v-icon v-else @click="isModuleList = !isModuleList">view_list</v-icon>
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
 
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
+      <ProductsContent :isModuleList="isModuleList"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+  import ProductsContent from '~/components/ProductsContent'
+
   export default {
     data () {
       return {
-        items: [
+        breadcrumbs: [
           {
             text: '',
             isHome: true,
@@ -76,24 +58,24 @@
       handleHash (hash) {
         switch (hash) {
           case '#all':
-            this.items[1].text = 'Вся продукция'
-            this.items[1].link = '/products#all'
-            this.items[1].disabled = false
+            this.breadcrumbs[1].text = 'Вся продукция'
+            this.breadcrumbs[1].link = '/products#all'
+            this.breadcrumbs[1].disabled = false
             break
           case '#saw-files':
-            this.items[1].text = 'Пилки и файлы'
-            this.items[1].link = '/products#saw-files'
-            this.items[1].disabled = false
+            this.breadcrumbs[1].text = 'Пилки и файлы'
+            this.breadcrumbs[1].link = '/products#saw-files'
+            this.breadcrumbs[1].disabled = false
             break
           case '#smart-disks':
-            this.items[1].text = 'Диски SMart'
-            this.items[1].link = '/products#smart-disks'
-            this.items[1].disabled = false
+            this.breadcrumbs[1].text = 'Диски SMart'
+            this.breadcrumbs[1].link = '/products#smart-disks'
+            this.breadcrumbs[1].disabled = false
             break
           case '#equipment':
-            this.items[1].text = 'Оборудование'
-            this.items[1].link = '/products#equipment'
-            this.items[1].disabled = false
+            this.breadcrumbs[1].text = 'Оборудование'
+            this.breadcrumbs[1].link = '/products#equipment'
+            this.breadcrumbs[1].disabled = false
             break
         }
       }
@@ -104,6 +86,7 @@
       }
     },
     components: {
+      ProductsContent
     }
   }
 </script>
@@ -111,6 +94,9 @@
 <style lang="stylus" scoped>
   .v-breadcrumbs li:nth-child(even) {
     padding: 0 6px;
+  }
+  .w-100 {
+    width: 100%;
   }
 
   @media screen and (max-width: 960px) {
