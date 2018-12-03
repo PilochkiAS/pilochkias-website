c315a5967758038a1b9ee9a685bbae8c07440618
OleMan <aom-95@live.com>
2018-12-03 16:57:45 +0200

    fix axios try #5

--- store/api/index.js
+++ store/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:3000'
+if (process.env.herokuBaseURL) {
+  axios.defaults.baseURL = 'https://pilochki.herokuapp.com'
+}
 
 const fetchProducts = async () => {
   const { data } = await axios('api/products')
